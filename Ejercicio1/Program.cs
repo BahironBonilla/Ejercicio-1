9667a33d6f9f1728cd8591559894929334581d40
BAHIRON <BAHIRON@LAPTOP-4F57VLJR>
2021-02-19 15:11:41 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1, nota2, nota3;
+            float prom ;
+            string click = "s";
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Escriba las notas: \n");
+                Console.WriteLine("nota 1 = ");
+                nota1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("nota 2 = ");
+                nota2 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("nota 3 = ");            
+                nota3 = Int32.Parse(Console.ReadLine());
+
+                prom = (nota1 + nota2 + nota3) / 3;
+
+                if ( prom < 3)
+                {
+                    Console.WriteLine("el estudiante reprobo su promedio es "+ prom);
+                }
+                else
+                {
+                    Console.WriteLine("El estudiante aprobo su promedio es " + prom);
+                }
+
+                Console.WriteLine("\n\n\n\n ¿Desea continuar?[s|n]");
+                click = Console.ReadLine();
+
+            } while (click == "s" || click == "S");
+
+
+
+
+        }
+    }
+}
